8fb110749d1e565f68165c7eb73def23d1c70df1
Astro-Boot <chirsbalc27@gmail.com>
2024-07-22 19:08:28 -0400

    Class #44

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,17 +1,45 @@
+import componentImage from "./assets/components.png";
+import reactImage from "./assets/react-core-concepts.png";
 
+let reactDescriptions = [
+  "Fundamental",
+  "Core",
+  "Crucial",
+  "Essential",
+  "Important",
+  "Key",
+  "Necessary",
+  "Needed",
+  "Primary",
+  "Vital",
+];
+function getRandomDescription() {
+  let randomIndex = Math.floor(Math.random() * reactDescriptions.length);
+  return reactDescriptions[randomIndex];
+}
 
-function Header(){
+function Header() {
+  let description = getRandomDescription();
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
+}
 
+function CoreConceptCard(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
 }
 
 function App() {
@@ -19,6 +47,19 @@ function App() {
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConceptCard
+              title="Component"
+              description="The core Ui building block."
+              img={componentImage}
+            />
+            <CoreConceptCard/>
+            <CoreConceptCard/>
+            <CoreConceptCard/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
@@ -26,3 +67,4 @@ function App() {
 }
 
 export default App;
+
