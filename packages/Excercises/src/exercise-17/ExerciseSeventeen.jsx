97ddd783320a8aa8820c9d36f138b0f69b9df1b0
Astro-Boot <chirsbalc27@gmail.com>
2024-08-03 12:51:12 -0400

    feat(packages/Excercises): Exercise 17 Completed

--- packages/Excercises/src/exercise-17/ExerciseSeventeen.jsx
+++ packages/Excercises/src/exercise-17/ExerciseSeventeen.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import './ExerciseSeventeen.css';
+
+const COLORS = {
+	Yes: 'green',
+	No: 'red',
+	default: 'white',
+};
+
 export default function ExerciseSeventeen() {
+	const [editText, setEditText] = useState('default');
+
+	const handleClick = (value) => setEditText(value);
+
 	return (
 		<div id='exercise-17'>
-			<h1>CSS is great!</h1>
+			<h1 style={{ color: COLORS[editText] }}>CSS is great!</h1>
 			<menu>
-				<li>
-					<button>Yes</button>
-				</li>
-				<li>
-					<button>No</button>
-				</li>
+				{['Yes', 'No'].map((option) => (
+					<li key={option}>
+						<button onClick={() => handleClick(option)}>{option}</button>
+					</li>
+				))}
 			</menu>
 		</div>
 	);
