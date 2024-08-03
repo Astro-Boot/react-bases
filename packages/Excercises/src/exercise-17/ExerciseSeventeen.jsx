6d11827499260c11808ec15251c79e01ad9c1b31
Astro-Boot <chirsbalc27@gmail.com>
2024-08-03 11:50:46 -0400

    feat(packages/Excercises): Add React Exercises

---
+++ packages/Excercises/src/exercise-17/ExerciseSeventeen.jsx
@@ -0,0 +1,16 @@
+import './ExerciseSeventeen.css';
+export default function ExerciseSeventeen() {
+	return (
+		<div id='exercise-17'>
+			<h1>CSS is great!</h1>
+			<menu>
+				<li>
+					<button>Yes</button>
+				</li>
+				<li>
+					<button>No</button>
+				</li>
+			</menu>
+		</div>
+	);
+}
---
+++ packages/Excercises/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ExerciseSeventeen from './exercise-17/ExerciseSeventeen';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		<ExerciseSeventeen></ExerciseSeventeen>
+	</React.StrictMode>,
+);
